8c55cdc11b1904758b9292f39a7cff80b240d46b
MihailTsybakov <62279777+MihailTsybakov@users.noreply.github.com>
2021-03-04 13:20:57 +0300

    Update Create.h

--- Create.h
+++ Create.h
@@ -2,6 +2,8 @@
 #define CREATE
 #include "CIntN.h"
 #include <vector>
+#include "factory.h"
+
 vector<string> split(string source, char separator); // Splits string by separator
 
 CIntN* Create_CINTN(string creation_parameters, CIntN_Factory);
